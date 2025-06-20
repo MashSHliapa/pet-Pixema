cf942f208a57b567f24499812f15b85e812bd116
@MashSHliapa <shliapa.maria@mail.ru>
2025-06-20 17:40:00 +0300

    feat(components): create Switch

---
+++ src/components/Switch/Switch.tsx
@@ -0,0 +1,9 @@
+import './Switch.scss';
+export function Switch() {
+  return (
+    <label className="switch">
+      <input type="checkbox" className="switch__input" />
+      <span className="switch__slider"></span>
+    </label>
+  );
+}
